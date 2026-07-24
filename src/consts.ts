5060cb2488f72fed88452fa55456dcafb7563f4e
Hafiz <huseynoff.hafiz@gmail.com>
2026-07-25 00:45:31 +0400

    initial

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Hafiz';
+export const SITE_DESCRIPTION = 'Builds web applications';
+export const SITE_TAGLINE = 'Web Developer';
+export const SITE_SUBTAGLINE =
+	"Building web applications, documenting good stuff";
